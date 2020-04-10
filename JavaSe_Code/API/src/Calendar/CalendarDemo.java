1d1583bcc8aefd8d8c4f6f7d590404a408285db0
LiangLiang723 <690428592@qq.com>
2020-04-10 18:05:31 +0800

    二月天

--- JavaSe_Code/API/src/Calendar/CalendarDemo.java
+++ JavaSe_Code/API/src/Calendar/CalendarDemo.java
@@ -1,7 +1,17 @@
 package Calendar;
 
+import java.util.Calendar;
+
 public class CalendarDemo {
     public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c);
 
+        c.set(2022,11,20);
+        c.add(Calendar.YEAR, 3);
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int date = c.get(Calendar.DATE);
+        System.out.println(year + "年" + month + "月" + date + "日");
     }
 }
